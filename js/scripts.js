2d842c5f7109f0207c4ea81bd398dbce13a6887d
Erik Zakrzewski <erik.zak.sf@gmail.com>
2017-08-25 11:43:12 -0700

    continue work on back end and front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 //Business logic
-
+var totalTop = "" ;
 
 function Pizza(size, toppings, total) {
   this.size = size;
   this.toppings = toppings;
-  this.total = total;
+  this.total = function functionName() {
+    return this.size + this.toppings;
+  }
 }
 
 Pizza.prototype.cost = function () {
-    return this.size =+ this.toppings = this.total
+    return this.size + this.toppings ;
 };
 
 
@@ -17,6 +19,11 @@ Pizza.prototype.cost = function () {
 
 $(document).ready(function() {
   $("#pizzaForm").submit(function () {
-    
-  })
-})
+    event.preventDefault();
+    debugger
+    $("input:checkbox[name=pizza-toppings]:checked").each(function() {
+      var Top = $(this).val();
+    });
+    $("input:select[name=size]:selected")
+  });
+});
