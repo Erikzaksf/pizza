58e7ec9df36f37a3586ca3ad10da81f5a5e0c6ff
Erik Zakrzewski <erik.zak.sf@gmail.com>
2017-09-03 14:28:46 -0700

    add working funtionality to my javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,33 @@
 //Business logic
+var totalPrice = 0;
 
-
-function Pizza(size, toppings, total) {
+function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
-  this.total = function cost() {
-    return this.size + this.toppings;
-  }
 }
-  var thePizza = new Pizza(0, [0], 0);
-//Pizza.prototype.cost = function () {
-//    return this.size + this.toppings ;
-// };
+Pizza.prototype.price = function (toppings, size) {
+  return totalPrice += this.toppings + this.size;
+}
+
+
+
 
 
 
 //User interface logic
 
 $(document).ready(function() {
-  var Top = "" ;
-//var thePizza = new Pizza(0, [0], 0);
-  $("#pizzaForm").submit(function () {
-    event.preventDefault();
-    debugger
-    $("input:checkbox[name=pepp]:checked").each(function() {
-    thePizza.toppings + $(this).val();
-    });
-    $("input:checkbox[name=mush]:checked").each(function() {
-    thePizza.toppings + $(this).val();
-    });
-    $("input:checkbox[name=onion]:checked").each(function() {
-    thePizza.toppings + $(this).val();
-    });
-    $("input:checkbox[name=cheese]:checked").each(function() {
-    thePizza.toppings + $(this).val();
-    });
 
+  var thePizza = new Pizza(0, 0);
+  $("#pizzaForm").submit(function() {
+    event.preventDefault();
+    $("input:checkbox:checked").each(function() {
+      thePizza.toppings +=  parseInt($(this).val());
+    });
     $("input:radio[name=size]:checked").each(function(){
-    thePizza.size = $(this).val();
+    thePizza.size =  parseInt($(this).val());
     console.log(thePizza);
     });
+    $('span#total').append('$' + thePizza.price(totalPrice)) ;
   });
 });
